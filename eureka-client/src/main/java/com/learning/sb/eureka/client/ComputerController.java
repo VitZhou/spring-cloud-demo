293ea87b5fd7a6c365401b25f9fd1e49b18af066
henryz <zsh989163@163.com>
2017-03-15 22:53:34 +0800

    add response excel demo

--- eureka-client/src/main/java/com/learning/sb/eureka/client/ComputerController.java
+++ eureka-client/src/main/java/com/learning/sb/eureka/client/ComputerController.java
@@ -8,6 +8,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 public class ComputerController {
 
@@ -17,5 +29,40 @@ public class ComputerController {
     public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
         logger.debug("a:{},b:{}", a, b);
         return a + b;
+    }
+
+    @RequestMapping(value = "/excel")
+    public byte[] getExcel(HttpServletResponse response) {
+        String fileName = "/home/henryz/javaProject/dede.xls";
+        File f = new File(fileName);
+
+        if (!f.exists()) {
+            logger.error("can't found file:{}", fileName);
+        }
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream((int) f.length());
+        BufferedInputStream in = null;
+        try {
+            in = new BufferedInputStream(new FileInputStream(f));
+            int buf_size = 1024;
+            byte[] buffer = new byte[buf_size];
+            int len = 0;
+            while (-1 != (len = in.read(buffer, 0, buf_size))) {
+                bos.write(buffer, 0, len);
+            }
+
+        } catch (IOException e) {
+            logger.error("fail to read file", e);
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+                bos.close();
+            } catch (IOException e) {
+                logger.error("fail to close stream", e);
+            }
+        }
+        return bos.toByteArray();
     }
 }
--- feign/src/main/java/com/learning/sb/feign/ComputeClientHystrix.java
+++ feign/src/main/java/com/learning/sb/feign/ComputeClientHystrix.java
@@ -3,11 +3,18 @@ package com.learning.sb.feign;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.ServletResponse;
+
 @Component
 public class ComputeClientHystrix implements ComputeClient {
 
     @Override
     public Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b) {
         return -100000;
+    }
+
+    @Override
+    public byte[] getExcel() {
+        return null;
     }
 }
