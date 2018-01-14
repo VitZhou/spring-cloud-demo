3ff8269c316fb9d3131b827f09af4aa21901562a
vitzhou <zsh989163@163.com>
2018-01-14 21:32:08 +0800

    ``

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ docker build -t zsh9891/spring-cloud-demo-eureka-consumer:latest eureka/consumer
 docker build -t zsh9891/spring-cloud-demo-eureka-producer:latest eureka/producer
 docker build -t zsh9891/spring-cloud-demo-gateway:latest gateway
 
-docker push zsh9891/spring-cloud-demo-eureka-server:latest
-docker push zsh9891/spring-cloud-demo-eureka-consumer:latest
-docker push zsh9891/spring-cloud-demo-eureka-producer:latest
-docker push zsh9891/spring-cloud-demo-gateway:latest
+#docker push zsh9891/spring-cloud-demo-eureka-server:latest
+#docker push zsh9891/spring-cloud-demo-eureka-consumer:latest
+#docker push zsh9891/spring-cloud-demo-eureka-producer:latest
+#docker push zsh9891/spring-cloud-demo-gateway:latest
 
 
 #minishift start --insecure-registry="192.168.31.34" --registry-mirror=https://e3wtejq8.mirror.aliyuncs.com/ --memory 4GB
